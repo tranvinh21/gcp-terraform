a8a7e199860344c93026879f791b0cea5bfc38f8
Trần Vinh <vinhtran.vicidev@gmail.com>
2024-12-16 19:35:22 +0700

    update config

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "gcs" {
-  name          = "my-bucket-ttssst2"
+  name          = "my-bucket-ttsss22t2"
   project       = "my-practice-443804"
   location      = "US"
   force_destroy = true
