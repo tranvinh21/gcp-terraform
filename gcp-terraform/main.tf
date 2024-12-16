694dbaa14ef06bce397a28dc5fb00e8b4d6aa2e8
Trần Vinh <97041326+tranvinh21@users.noreply.github.com>
2024-12-16 19:14:28 +0700

    Delete gcp-terraform directory

--- gcp-terraform/main.tf
+++
@@ -1,7 +0,0 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-demo"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-
-}
