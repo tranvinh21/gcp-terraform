5f7af33c5144381c7044c2d65040891469e8c136
Trần Vinh <vinhtran.vicidev@gmail.com>
2024-12-16 19:16:38 +0700

    ok

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "google_storage_bucket" "my-bucket" {
+  name                     = "tt-demo"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
+
